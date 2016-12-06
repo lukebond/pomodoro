4ee21c05ab2dc715b463200d95abe76829a1a433
Luke Bond <luke.n.bond@gmail.com>
2016-12-06 15:15:29 +0000

    fix: fixed incorrect use of & that caused sound to play at the beginning of the sesion rather than the end

--- pomodoro.sh
+++ pomodoro.sh
@@ -4,4 +4,4 @@ TIME=${TIME:-1500}
 TITLE="${TITLE:-Pomodoro Timer}"
 MESSAGE="${MESSAGE:-¡Time for a break!        }"
 notify-send -t 5000 -i /usr/share/pixmaps/pomodoro-counter-empty.png "${TITLE}" "¡Focus session started!   "
-sleep "${TIME}" && notify-send -t 10000 -i /usr/share/pixmaps/pomodoro-counter-empty.png "${TITLE}" "${MESSAGE}" & gst123 -q /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
+sleep "${TIME}" && (notify-send -t 10000 -i /usr/share/pixmaps/pomodoro-counter-empty.png "${TITLE}" "${MESSAGE}" & gst123 -q /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga)
