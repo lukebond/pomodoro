e800b66778026093ccaa65deb37c07372c341ab3
Luke Bond <luke.n.bond@gmail.com>
2016-12-06 11:32:33 +0000

    feat: added sound, and a startup message

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set +x
 TIME=${TIME:-1500}
 TITLE="${TITLE:-Pomodoro Timer}"
 MESSAGE="${MESSAGE:-¡Time for a break!        }"
-sleep "${TIME}" && notify-send -t 10000 -i /usr/share/pixmaps/pomodoro-counter-empty.png "${TITLE}" "${MESSAGE}"
+notify-send -t 5000 -i /usr/share/pixmaps/pomodoro-counter-empty.png "${TITLE}" "¡Focus session started!   "
+sleep "${TIME}" && notify-send -t 10000 -i /usr/share/pixmaps/pomodoro-counter-empty.png "${TITLE}" "${MESSAGE}" & gst123 -q /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
